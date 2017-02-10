8f91a2b1bc1f3f8d35e4842557c31390964359c6
Happa8 <okadakoutaro@gmail.com>
2017-02-11 00:41:26 +0900

    First Commit

--- Kemono_Friends_WPF/MainWindow.xaml.cs
+++ Kemono_Friends_WPF/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kemono_Friends_WPF
 {
-	/// <summary>
-	/// MainWindow.xaml の相互作用ロジック
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		public string Specialty;
+		public string[] FriendsGoroku = new string[] { "わーい", "すごーい", "うわー！" };
+
+		private void KeyDownHandler(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				Specialty = specialtyTextbox.Text;
+
+				System.Random r = new System.Random();
+				string Word = FriendsGoroku[r.Next(4)] + "\n" + "君は" + Specialty + "が得意なフレンズなんだね！";
+				Hukidashi.Text = Word;
+			}
 		}
 	}
 }
